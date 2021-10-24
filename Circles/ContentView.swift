371dd0a1aa21b93d13cafea5774d32fa97b5f8f7
Fernando <fernando.eb@icloud.com>
2021-10-24 12:39:18 -0300

    animaciones de circulos
    animaciones de círculos que imitan a la app de respirar de apple watch

--- Circles/ContentView.swift
+++ Circles/ContentView.swift
@@ -1,16 +1,65 @@
-//
-//  ContentView.swift
-//  Circles
-//
-//  Created by Fernando on 24-10-21.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var escalado = false
+    @State private var rotación = false
+    @State private var movimiento = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Rectangle().edgesIgnoringSafeArea(.all)
+            ZStack{
+                
+                //MARK: - Circles step 1
+                ZStack{
+                    Circle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [.mint, .white]), startPoint: .top, endPoint: .bottom))
+                        .frame(width: 120, height: 120)
+                        .offset(y: movimiento ? -60 : 0)
+                    Circle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [.mint, .white]), startPoint: .bottom, endPoint: .top))
+                        .frame(width: 120, height: 120)
+                        .offset(y: movimiento ? 60 : 0)
+                }.opacity(0.6)
+                
+                //MARK: - Circles step 2
+                ZStack{
+                    Circle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [.mint, .white]), startPoint: .top, endPoint: .bottom))
+                        .frame(width: 120, height: 120)
+                        .offset(y: movimiento ? -60 : 0)
+                    Circle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [.mint, .white]), startPoint: .bottom, endPoint: .top))
+                        .frame(width: 120, height: 120)
+                        .offset(y: movimiento ? 60 : 0)
+                }.opacity(0.6)
+                    .rotationEffect(.degrees(60))
+                
+                
+                //MARK: - Circles step 3
+                ZStack{
+                    Circle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [.mint, .white]), startPoint: .top, endPoint: .bottom))
+                        .frame(width: 120, height: 120)
+                        .offset(y: movimiento ? -60 : 0)
+                    Circle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [.mint, .white]), startPoint: .bottom, endPoint: .top))
+                        .frame(width: 120, height: 120)
+                        .offset(y: movimiento ? 60 : 0)
+                }.opacity(0.6)
+                    .rotationEffect(.degrees(120))
+                
+                //MARK: animations
+            }.rotationEffect(.degrees(rotación ? 90 : 0))
+//                .scaleEffect(escalado ? 1 : 1/4)
+                .animation(Animation.easeInOut.repeatForever(autoreverses: true).speed(1/8), value: escalado ? 1 : 1/4)
+                .onAppear {
+                    movimiento.toggle()
+                    rotación.toggle()
+                    escalado.toggle()
+                }
+        }
     }
 }
 
