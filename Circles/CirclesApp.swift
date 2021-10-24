89f19ff83e6f9c152071204ee94f9d7d7635d312
Fernando <fernando.eb@icloud.com>
2021-10-24 11:58:18 -0300

    Initial Commit

---
+++ Circles/CirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CirclesApp.swift
+//  Circles
+//
+//  Created by Fernando on 24-10-21.
+//
+
+import SwiftUI
+
+@main
+struct CirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
